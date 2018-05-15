b77f6b0b50ece93c3612c826d8faa3c557092ff9
sajmo0n <jedlinskisz@gmail.com>
2018-05-15 17:04:55 +0200

    cos

---
+++ cmake-build-debug/CMakeFiles/Kolejka.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kolejka.dir/src/sources/main.cpp.o"
+  "Kolejka.pdb"
+  "Kolejka"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kolejka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
